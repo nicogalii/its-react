40004568ddcd045d8a7e0479abb6c66237ef04e8
nicogalii <galizianicolo2004@gmail.com>
2024-11-27 23:22:19 +0100

    esercitazione con count, lap e date

---
+++ esercitazione-counter/src/App.tsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [lap, setLap] = useState(0);
+
+  // Array di numeri inizializzato come un array vuoto
+  const [time, setTime] = useState<number[]>([]);
+
+  /**
+   * Funzione per incrementare il valore del contatore 'count' di 1
+   */
+  function counter() {
+    setCount(count + 1);
+  }
+
+  /**
+   * Funzione per incrementare il valore del contatore 'lap' di 1 se il contatore 'count è pari
+   */
+  function counterLap() {
+    // Verifica se il contatore 'count' è pari
+    if (count % 2 === 0) {
+      setLap(lap + 1);
+    }
+  }
+
+  /**
+   * Funzione per aggiungere all'array 'time' la data corrente
+   */
+  function addArray() {
+    setTime([...time, Date.now()]);
+  }
+
+  return (
+    <>
+      <p>Lap: {lap}</p>
+      <p>Count: {count}</p>
+
+      {/* Bottone per aumentare il count ad ogni click*/}
+      <button
+        onClick={() => {
+          counter();
+          counterLap();
+          addArray();
+        }}
+      >
+        +1
+      </button>
+
+      {/* Lista con le date dei click dei click */}
+      <ul>
+        {time.map((currentTime, index) => {
+          return <li key={index}>{new Date(currentTime).toLocaleString()}</li>;
+        })}
+      </ul>
+    </>
+  );
+}
+
+export default App;
