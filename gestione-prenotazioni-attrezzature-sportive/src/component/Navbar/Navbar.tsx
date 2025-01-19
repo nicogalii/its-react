d9a5393612a454c091d6741c2e9c74f2ec120d76
nicogalii <galizianicolo2004@gmail.com>
2025-01-20 00:00:30 +0100

    edit

--- gestione-prenotazioni-attrezzature-sportive/src/component/Navbar/Navbar.tsx
+++ gestione-prenotazioni-attrezzature-sportive/src/component/Navbar/Navbar.tsx
@@ -1,34 +1,62 @@
+import { NavLink } from "react-router";
 import "./Navbar.css";
 
 const Navbar = () => {
   return (
     <>
       <header>
-        <div className="logo">
-          <img src="./assets/images/logo.svg" alt="Logo di ..." />
-        </div>
+        {/* Logo */}
+        <a href="./" className="logo">
+          <img
+            src="../../../assets/images/logo.svg"
+            alt="Logo di elite equipment"
+          />
+        </a>
         <nav>
-          {/* <input type="checkbox" id="check" />
           <label htmlFor="check" className="checkbtn">
             <img
-              src="./assets/svg/hamburger.svg"
+              src="../../../assets/images/menu-hamburger.svg"
               alt="Menu di navigazione"
               id="hamburger"
             />
-          </label> */}
-          {/* <div className="nav-mobile"> */}
-          <ul>
-            <li>
-              <a href="/">Home</a>
-            </li>
-            <li>
-              <a href="/equipment-booked">Equipment Booked</a>
-            </li>
-            <li>
-              <a href="/login">Login o Registrazione</a>
-            </li>
-          </ul>
-          {/* </div> */}
+          </label>
+          <input type="checkbox" id="check" />
+
+          {/* Mobile Menu */}
+          <div className="nav-mobile">
+            <ul>
+              {/* Link alla Home */}
+              <li>
+                <NavLink to="/">Home</NavLink>
+              </li>
+              {/* Link alle prenotazioni */}
+              <li>
+                <NavLink to="/equipment-booked">Equipment Booked</NavLink>
+              </li>
+              {/* Link alle registrazioni */}
+              <li>
+                <NavLink to="/login">Login o Registrazione</NavLink>
+              </li>
+            </ul>
+          </div>
+
+          {/* Desktop Menu */}
+          <div className="nav-desktop">
+            <ul>
+              {/* Link alla Home */}
+              <li>
+                <NavLink to="/">Home</NavLink>
+              </li>
+              {/* Link alle prenotazioni */}
+              <li>
+                <NavLink to="/equipment-booked">Equipment Booked</NavLink>
+              </li>
+              {/* Link alle registrazioni */}
+              <li>
+                <NavLink to="/login">Login o Registrazione</NavLink>
+              </li>
+            </ul>
+          </div>
         </nav>
       </header>
     </>
